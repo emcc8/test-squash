13258fa197bd1defa70205be765265546be1cb63
emcc8 <90630255+emcc8@users.noreply.github.com>
2021-10-25 11:50:36 +0100

    JIRA-Bugfix-007

--- src/main/java/com/example/gitdemo/GitDemoApplication.java
+++ src/main/java/com/example/gitdemo/GitDemoApplication.java
@@ -13,8 +13,8 @@ public class GitDemoApplication {
         SpringApplication.run(GitDemoApplication.class, args);
     }
 
-    @GetMapping("/hello")
+    @GetMapping("/hello-spring")
     String greet() {
-        return "Hello, World!";
+        return "Hello, Spring!";
     }
 }
